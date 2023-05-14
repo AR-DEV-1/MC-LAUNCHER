f253b0a3ea65e573b859d90c5ee549dc1f068748
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-05-14 13:30:31 +0500

    Create launch.cs

---
+++ launch.cs
@@ -0,0 +1,7 @@
+JButton launchButton = new JButton("Launch Minecraft");
+launchButton.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        // Launch Minecraft code here
+    }
+});
+add(launchButton);
